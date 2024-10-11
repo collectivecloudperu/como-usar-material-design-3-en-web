ce96ad1f270d14515e0e71b9cc488b529e131994
collectivecloudperu <collectivecloudperu@gmail.com>
2024-10-11 06:46:22 -0500

    Cambios

---
+++ material-design-3-react/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Importamos 2 estilos del componente Botón de Material Design 3
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+
+        {/* Botones creados con Material Design 3 */}
+        <md-outlined-button>Anterior</md-outlined-button>
+        <br/>
+        <br/>
+        <md-filled-button>Siguiente</md-filled-button>
+        {/* Botones creados con Material Design 3 */}
+
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
